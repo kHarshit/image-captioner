98357ccac2138a9bd54f6a950806bbdbfa992150
Harshit Kumar <kumar_harshit@outlook.com>
2019-01-06 00:36:07 +0530

    fix: Add heroku url to allowed host

--- image_captioner/settings.py
+++ image_captioner/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'imagecaptioner.herokuapp.com']
 
 
 # Application definition
