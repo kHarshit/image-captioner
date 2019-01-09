965c9b61eddd83219c8b868de8371443a25a8139
Harshit Kumar <kumar_harshit@outlook.com>
2019-01-09 19:26:18 +0530

    fix: css not loading on heroku by adding whitenoise middleware

--- image_captioner/settings.py
+++ image_captioner/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'image_captioner.urls'
@@ -126,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 # python manage.py collectstatic
+# https://devcenter.heroku.com/articles/django-assets
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
