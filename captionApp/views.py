e69ec19e762d9ed08b37cd43b49b80e099db7018
Harshit Kumar <kumar_harshit@outlook.com>
2019-04-13 16:16:13 +0530

    fix: revert views for github

--- captionApp/views.py
+++ captionApp/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-# import copy
+import copy
 from .forms import FileForm
-# from .models import File
-# from .prediction import *
+from .models import File
+from .prediction import *
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#
-#
-# class CustomUnpickler(pickle.Unpickler):
-#
-#     def find_class(self, module, name):
-#         if name == 'Vocabulary':
-#             return Vocabulary
-#         return super().find_class(module, name)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# vocab = CustomUnpickler(open('/home/harshit/Downloads/vocab.pkl', 'rb')).load()
+class CustomUnpickler(pickle.Unpickler):
+
+    def find_class(self, module, name):
+        if name == 'Vocabulary':
+            return Vocabulary
+        return super().find_class(module, name)
+
+
+vocab = CustomUnpickler(open('/home/harshit/Downloads/vocab.pkl', 'rb')).load()
 
 
 def handle_file(image):
@@ -26,45 +26,29 @@ def handle_file(image):
     pass
 
 
-# def index(request):
-#     """homepage"""
-#     if request.method == 'POST':
-#         form = FileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             myfile = File(file=request.FILES['file'])
-#             form.save()
-#             file_name = myfile.file  # file attribute of File model object
-#             print(file_name)
-#             file_name_original = copy.copy(file_name)  # prevent name modification after saving file
-#             handle_file(myfile)
-#             # process image
-#             output = process(file_name,
-#                              '/home/harshit/Downloads/pretrained_model/encoder-5-3000.pkl',
-#                              '/home/harshit/Downloads/pretrained_model/decoder-5-3000.pkl',
-#                              vocab,
-#                              256, 512, 1)
-#             print(output)
-#             myfile.save()
-#             print(file_name)
-#             return render(request, 'captionApp/output.html', {'file_name': file_name,
-#                                                               'file_name_original': file_name_original,
-#                                                               'output': output})
-#             # return HttpResponse('File successfully uploaded!')
-#         # else:
-#         #     return HttpResponse('Something Went Wrong!')
-#     else:
-#         form = FileForm()
-#     return render(request, 'captionApp/index.html', {'form': form})
-
 def index(request):
     """homepage"""
     if request.method == 'POST':
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
-            myfile = request.FILES['file']
-            print(myfile)
+            myfile = File(file=request.FILES['file'])
+            form.save()
+            file_name = myfile.file  # file attribute of File model object
+            print(file_name)
+            file_name_original = copy.copy(file_name)  # prevent name modification after saving file
             handle_file(myfile)
-            return render(request, 'captionApp/output.html', {'file': myfile})
+            # process image
+            output = process(file_name,
+                             '/home/harshit/Downloads/pretrained_model/encoder-5-3000.pkl',
+                             '/home/harshit/Downloads/pretrained_model/decoder-5-3000.pkl',
+                             vocab,
+                             256, 512, 1)
+            print(output)
+            myfile.save()
+            print(file_name)
+            return render(request, 'captionApp/output.html', {'file_name': file_name,
+                                                              'file_name_original': file_name_original,
+                                                              'output': output})
             # return HttpResponse('File successfully uploaded!')
         # else:
         #     return HttpResponse('Something Went Wrong!')
@@ -73,4 +57,19 @@ def index(request):
     return render(request, 'captionApp/index.html', {'form': form})
 
 
-
+### For heroku  ###
+# def index(request):
+#     """homepage"""
+#     if request.method == 'POST':
+#         form = FileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             myfile = request.FILES['file']
+#             print(myfile)
+#             handle_file(myfile)
+#             return render(request, 'captionApp/output.html', {'file': myfile})
+#             # return HttpResponse('File successfully uploaded!')
+#         # else:
+#         #     return HttpResponse('Something Went Wrong!')
+#     else:
+#         form = FileForm()
+#     return render(request, 'captionApp/index.html', {'form': form})
